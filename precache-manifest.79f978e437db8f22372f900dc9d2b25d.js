a4e1ec982080ed8b9b93a6b7149e61f16452a538
Ellie Yelizarieva <ellie@Ellies-MacBook-Pro.local>
2020-03-30 17:17:42 +0300

    Updates

---
+++ precache-manifest.79f978e437db8f22372f900dc9d2b25d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c7b96ac933a4c85e44f5348822bc0e",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "6df3042cb457ff0a60f1",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.e471fb58.chunk.css"
+  },
+  {
+    "revision": "7d80a93537d751d8b769",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.b4148bd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.b4148bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df3042cb457ff0a60f1",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.96c17921.chunk.js"
+  },
+  {
+    "revision": "09ea1b51af15d57aff1c",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.7ade0326.js"
+  }
+]);
